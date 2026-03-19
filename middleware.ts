51a9092e5b16bd27109d9def6f1992e7e65c1581
Vatsal <trivedivatsal63@gmail.com>
2026-03-19 22:07:15 +0530

    fix clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/chat(.*)'])
-
-export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) {
-        await auth.protect()
-    }
-})
+export default clerkMiddleware();
 
 export const config = {
-    matcher: [
-        '/((?!.*\\..*|_next).*)',
-        '/',
-        '/(api|trpc)(.*)',
-    ],
-}
+    matcher: ["/((?!_next|.*\\..*).*)"],
+};
